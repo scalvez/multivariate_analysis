4788e9ef1de3e983b48ee996fbb79ae2ef57a784
Steven Calvez <calvez.steven@gmail.com>
2016-03-15 19:14:20 +0100

    Fix script

--- script_application.sh
+++ script_application.sh
@@ -1,19 +1,24 @@
-1#!/bin/bash
+#!/bin/bash
 
 (
     for isotope_str in 0nu 2nu tl208 bi214
     do
         echo "Isotope : $isotope_str"
-        if $isotope_str -eq 2nu; then
+
+        isotope=$isotope_str
+        bb='2nu'
+        if [ $isotope=$bb ];
+        then
             sed -i -e 's@.*Long64.*@for (Long64_t ievt=0; ievt<20000;ievt++) {@g' classification_application.C
         fi
-
 
         sed -i -e 's@.*TString fname.*@TString fname = "./root_export_'$isotope_str'_25G.root";@g' classification_application.C
         sed -i -e 's@.*bdt_score.*@TFile *target = new TFile ("bdt_score_'$isotope_str'.root","RECREATE");@g' classification_application.C
 
         root -l -q classification_application.C
-        if $isotope_str -eq 2nu; then
+
+        if [ $isotope=$bb ];
+        then
             sed -i -e 's@.*Long64.*@for (Long64_t ievt=0; ievt<theTree->GetEntries();ievt++) {@g' classification_application.C
         fi
 
