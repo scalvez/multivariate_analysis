7a8310d4049aeb6cf8cc4fd56113eca631c34421
Steven Calvez <calvez.steven@gmail.com>
2016-03-15 19:35:58 +0100

    Actually fixing the script (needed spaces on each side of the comparator)

--- script_application.sh
+++ script_application.sh
@@ -7,18 +7,16 @@
 
         isotope=$isotope_str
         bb='2nu'
-        if [ $isotope=$bb ];
-        then
+        if [ "$isotope" = "$bb" ]; then
             sed -i -e 's@.*Long64.*@for (Long64_t ievt=0; ievt<20000;ievt++) {@g' classification_application.C
-        fi
+       fi
 
         sed -i -e 's@.*TString fname.*@TString fname = "./root_export_'$isotope_str'_25G.root";@g' classification_application.C
         sed -i -e 's@.*bdt_score.*@TFile *target = new TFile ("bdt_score_'$isotope_str'.root","RECREATE");@g' classification_application.C
 
         root -l -q classification_application.C
 
-        if [ $isotope=$bb ];
-        then
+        if [ $isotope = $bb ]; then
             sed -i -e 's@.*Long64.*@for (Long64_t ievt=0; ievt<theTree->GetEntries();ievt++) {@g' classification_application.C
         fi
 
