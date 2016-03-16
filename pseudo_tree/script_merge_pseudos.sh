81035a99a77dcae12da16c71b50e934a8a401f13
Steven Calvez <calvez.steven@gmail.com>
2016-03-16 19:46:25 +0100

    Fix merge script for 2nu also

--- pseudo_tree/script_merge_pseudos.sh
+++ pseudo_tree/script_merge_pseudos.sh
@@ -4,15 +4,15 @@
     shopt -s nullglob #empty arrays if no match
     tl_files=(tl208/*)
     bi_files=(bi214/*)
-    # two_nu_files=(2nu/*)
+    two_nu_files=(2nu/*)
     for i in {0..99}
     do
         tl=${tl_files[$i]}
         bi=${bi_files[$i]}
-        # two_nu=${two_nu_files[$i]}
+        two_nu=${two_nu_files[$i]}
         output="merged/pseudo_$i.root"
-        # command='~/.macros/merge_trees.C("snemodata",'$output','$two_nu','$bi','$tl')'
-        command='~/.macros/merge_trees.C("snemodata","'$output'","'$bi'","'$tl'")'
+        command='~/.macros/merge_trees.C("snemodata","'$output'","'$two_nu'","'$bi'","'$tl'")'
+        # command='~/.macros/merge_trees.C("snemodata","'$output'","'$bi'","'$tl'")'
         # echo $command
         root -l -q $command
     done
