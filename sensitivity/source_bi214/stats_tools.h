12ba8508a7e08498ffc3f88bea979c8fde01de0e
Steven Calvez <calvez.steven@gmail.com>
2016-05-04 15:03:45 +0200

    Add a new function to replace log of factorial because the integer limit was reached

--- sensitivity/source_bi214/stats_tools.h
+++ sensitivity/source_bi214/stats_tools.h
@@ -4,6 +4,18 @@ int factorial(int n)
   return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
 }
 
+int log_factorial(int n)
+{
+  if (n == 1 || n == 0)
+    return 0;
+
+  double s = 0;
+  for(unsigned int i = 2; i <= n; ++i) {
+    s += log(n);
+  }
+  return s;
+}
+
 double Poisson_probability(int n_obs, int n_exp)
 {
   return pow(n_exp,n_obs)*exp(-1*n_exp)/factorial(n_obs);
