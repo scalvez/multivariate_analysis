a7f9861eaeb821e91a30465f6e85ce1b715aaa2c
Steven Calvez <calvez.steven@gmail.com>
2016-05-04 16:47:59 +0200

    Cleaning and added code to check the fit agreement with pseudo data

--- sensitivity/source_bi214/source_bi214_fit.C
+++ sensitivity/source_bi214/source_bi214_fit.C
@@ -22,68 +22,60 @@
 #include <stdlib.h>
 
 #include "stats_tools.h"
-
-double likelihood(double activity)
-{
-  TFile * f_bi214 = TFile::Open("bi214_pdf.root");
-
-  TH1F *bi214_alpha_track_length_pdf = (TH1F*)f_bi214->Get("h");
-
-  TFile * f_bi214_pseudo = TFile::Open("pseudo.root");
-
-  TH1F *bi214_alpha_track_length_pseudo = (TH1F*)f_bi214_pseudo->Get("h");
-
-  double channel_1e1a_efficiency = 10733./2500000;
-
-  double f = 0.;
-
-  for(int i = 0; i < 100; i++)
-    {
-      double b = activity*channel_1e1a_efficiency*bi214_alpha_track_length_pdf->GetBinContent(i)*7*3.14e7*2.5;
-      int d = bi214_alpha_track_length_pseudo->GetBinContent(i);
-      // std::cout << "bin " << i << "  :  b = " << b << "  d = " << d << std::endl;
-      // std::cout << "bin likelihood " << 2*(b-d*log(b+0.1)+log(factorial(d))) << std::endl;
-      f += 2*(b-d*log(b+0.1)+log_factorial(d));
-    }
-
-  // std::cout << " DEBUG " << activity*1e6 << "  " << f << std::endl;
-  return f;
-
-}
+#include "sensitivity_constants.h"
 
 void fcn_to_minimize(int& npar, double* deriv, double& f, double par[], int flag)
 {
   TFile * f_bi214 = TFile::Open("bi214_pdf.root");
+  TH1F *bi214_pdf = (TH1F*)f_bi214->Get("h");
 
-  TH1F *bi214_alpha_track_length_pdf = (TH1F*)f_bi214->Get("h");
+  TFile * f_tl208 = TFile::Open("tl208_pdf.root");
+  TH1F *tl208_pdf = (TH1F*)f_tl208->Get("h");
 
-  TFile * f_bi214_pseudo = TFile::Open("pseudo.root");
-
-  TH1F *bi214_alpha_track_length_pseudo = (TH1F*)f_bi214_pseudo->Get("h");
-
-  double channel_1e1a_efficiency = 10733./2500000;
+  TFile * f_pseudo = TFile::Open("pseudo.root");
+  TH1F *pseudo = (TH1F*)f_pseudo->Get("h");
 
   f = 0.;
-  // std::cout << " -------- For activity = " << par[0] << std::endl;
-  // std::cout << " debug " << par[0] << "  " << channel_1e1a_efficiency << "  " << bi214_alpha_track_length_pdf->GetBinContent(38) << std::endl;
-  // std::cout << bi214_alpha_track_length_pseudo->GetBinContent(38) << std::endl;
   for(int i = 0; i < 100; i++)
     {
-      double b = par[0]*channel_1e1a_efficiency*bi214_alpha_track_length_pdf->GetBinContent(i)*7*3.14e7*2.5;
-      double d = bi214_alpha_track_length_pseudo->GetBinContent(i);
-      // std::cout << "bin " << i << "  :  b = " << b << "  d = " << d << std::endl;
+      double b = (par[0]*bi214_pdf->GetBinContent(i)*bi214_channel_1e1g_efficiency
+                  +par[1]*tl208_pdf->GetBinContent(i)*tl208_channel_1e1g_efficiency)*7*3.14e7*2.5;
+      double d = pseudo->GetBinContent(i);
       f += 2*(b-d*log(b+0.1)+log_factorial(d));
     }
 
-  // std::cout << " DEBUG " << par[0] << "  " << f << std::endl;
   return;
 
 }
 
+// double likelihood(double activity)
+// {
+//   TFile * f_bi214 = TFile::Open("bi214_pdf.root");
+
+//   TH1F *bi214_alpha_track_length_pdf = (TH1F*)f_bi214->Get("h");
+
+//   TFile * f_bi214_pseudo = TFile::Open("pseudo.root");
+
+//   TH1F *bi214_alpha_track_length_pseudo = (TH1F*)f_bi214_pseudo->Get("h");
+
+//   double f = 0.;
+
+//   for(int i = 0; i < 100; i++)
+//     {
+//       double b = activity*bi214_channel_1e1a_efficiency*bi214_alpha_track_length_pdf->GetBinContent(i)*7*3.14e7*2.5;
+//       int d = bi214_alpha_track_length_pseudo->GetBinContent(i);
+//       // std::cout << "bin " << i << "  :  b = " << b << "  d = " << d << std::endl;
+//       // std::cout << "bin likelihood " << 2*(b-d*log(b+0.1)+log(factorial(d))) << std::endl;
+//       f += 2*(b-d*log(b+0.1)+log_factorial(d));
+//     }
+
+//   // std::cout << " DEBUG " << activity*1e6 << "  " << f << std::endl;
+//   return f;
+// }
+
 void source_bi214_fit()
 {
-
-  const int npar = 1;
+  const int npar = 2;
 
   TMinuit minuit(npar);
 
@@ -95,34 +87,114 @@ void source_bi214_fit()
   double maxVal[npar];            // maximum bound on parameter
   string parName[npar];           // parameter name
 
-  par[0]= 0.000425;
+  par[0]= 100./1e6;
   stepSize[0] = 0.00001;
   minVal[0] = 0.00001;
   maxVal[0] = 0.001;
   // parName[0] = "activity in uBq/kg";
-  parName[0] = "activity";
+  parName[0] = "activity bi214";
 
   minuit.DefineParameter(0, parName[0].c_str(),
                          par[0], stepSize[0], minVal[0], maxVal[0]);
 
+  par[1]= 20./1e6;
+  stepSize[1] = 0.00001;
+  minVal[1] = 0.00001;
+  maxVal[1] = 0.001;
+  // parName[1] = "activity in uBq/kg";
+  parName[1] = "activity tl208";
+
+  minuit.DefineParameter(1, parName[1].c_str(),
+                         par[1], stepSize[1], minVal[1], maxVal[1]);
+
   minuit.Migrad();
 
-  double activity = 0;
-  double activity_err = 0;
-  minuit.GetParameter(0,activity,activity_err);
+  double activity_bi214 = 0;
+  double activity_bi214_err = 0;
+  minuit.GetParameter(0,activity_bi214,activity_bi214_err);
 
-  std::cout << " Measured activity is : " << activity*1e6 << " +/- " << activity_err*1e6 << " uBq/kg" << std::endl;
+  double activity_tl208 = 0;
+  double activity_tl208_err = 0;
+  minuit.GetParameter(1,activity_tl208,activity_tl208_err);
 
+  std::cout << " Measured bi214 activity is : " << activity_bi214*1e6 << " +/- " << activity_bi214_err*1e6 << " uBq/kg" << std::endl;
+  std::cout << " Measured tl208 activity is : " << activity_tl208*1e6 << " +/- " << activity_tl208_err*1e6 << " uBq/kg" << std::endl;
 
-  TGraph *g_likelihood = new TGraph(100);
-  double activity_start = 1e-5;
-  double activity_end = 1e-3;
+  // TGraph *g_likelihood = new TGraph(100);
+  // double activity_start = 1e-5;
+  // double activity_end = 1e-3;
 
-  for (unsigned int i = 0; i<100;++i) {
-    double activity = activity_start + i*(activity_end-activity_start)/100.;
-    double Q = likelihood(activity);
-    g_likelihood->SetPoint(i,activity*1e6,Q);
-  }
-  g_likelihood->Draw();
+  // for (unsigned int i = 0; i<100;++i) {
+  //   double activity = activity_start + i*(activity_end-activity_start)/100.;
+  //   double Q = likelihood(activity);
+  //   g_likelihood->SetPoint(i,activity*1e6,Q);
+  // }
+  // g_likelihood->Draw();
+
+  TFile * f_bi214 = TFile::Open("bi214_pdf.root");
+  TH1F *bi214_pdf = (TH1F*)f_bi214->Get("h");
+
+  bi214_pdf->SetLineColor(kOrange);
+  bi214_pdf->SetFillColor(kOrange);
+
+  TFile * f_tl208 = TFile::Open("tl208_pdf.root");
+  TH1F *tl208_pdf = (TH1F*)f_tl208->Get("h");
+  tl208_pdf->SetLineColor(kGreen+1);
+  tl208_pdf->SetFillColor(kGreen+1);
+
+  TFile * f_pseudo = TFile::Open("pseudo.root");
+  TH1F *pseudo = (TH1F*)f_pseudo->Get("h");
+  pseudo->SetLineColor(kBlack);
+  // pseudo->SetMarkerStyle(1);
+
+  THStack *hs = new THStack("hs","Energy [keV]");
+  bi214_pdf->Scale(activity_bi214*bi214_channel_1e1g_efficiency*mass*exposure);
+  tl208_pdf->Scale(activity_tl208*tl208_channel_1e1g_efficiency*mass*exposure);
+
+  hs->Add(bi214_pdf);
+  hs->Add(tl208_pdf);
+
+  TCanvas *c1 = new TCanvas("c1","example",600,700);
+
+  TPad *pad1 = new TPad("pad1","pad1",0,0.3,1,1);
+  // pad1->SetBottomMargin(0.05);
+  pad1->SetTopMargin(0.03);
+  pad1->Draw();
+  pad1->cd();
+
+  hs->DrawClone();
+
+  pseudo->DrawClone("samePE");
+  c1->cd();
+
+  // TH1 *h_sum = new TH1F("h_sum","h_sum");
+
+  bi214_pdf->Sumw2();
+  bi214_pdf->Add(tl208_pdf);
+  //Error computation to check
+  // bi214_pdf->Sumw2();
+  pseudo->Sumw2();
+
+  TPad *pad2 = new TPad("pad2","pad2",0,0,1,0.3);
+  pad2->SetTopMargin(0.0);
+  // pad2->SetBottomMargin(0.05);
+  pad2->Draw();
+  pad2->cd();
+
+  bi214_pdf->SetStats(0);
+  pseudo->SetStats(0);
+
+  pseudo->Divide(bi214_pdf);
+
+  pseudo->GetYaxis()->SetRangeUser(0.6,1.4);
+  pseudo->Draw("ep");
+
+  TLine *line = new TLine(0,1,5,1);
+  // line->SetLineColor(kBlack);
+  line->SetLineStyle(kDashed);
+  line->Draw("same");
+
+  c1->cd();
+
   return;
 }
