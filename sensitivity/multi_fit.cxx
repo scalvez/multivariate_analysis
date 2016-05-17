86b73da070cf729e99756e537c1b991bba8be9f5
Steven Calvez <calvez.steven@gmail.com>
2016-05-17 23:29:53 +0200

    Issue with Minuit linking

--- sensitivity/multi_fit.cxx
+++ sensitivity/multi_fit.cxx
@@ -1,55 +1,50 @@
-#include "Riostream.h"
-#include "TMath.h"
 #include "TH1.h"
-#include "THStack.h"
-#include "TGraph.h"
-#include "TMultiGraph.h"
-#include "TGraphErrors.h"
-#include "TF1.h"
-#include "TH2.h"
-#include "TCanvas.h"
-#include "TLine.h"
-#include "TLine.h"
-#include "TLegend.h"
 #include "TFile.h"
-#include "TStyle.h"
 #include "TMinuit.h"
+#include "TSystem.h"
 #include <vector>
+#include <string>
 #include <iostream>
-#include <fstream>
-#include <time.h>
-#include <math.h>
-#include <stdlib.h>
 
 #include "stats_tools.h"
 #include "sensitivity_constants.h"
+#include "analysis_config.h"
+#include "multi_fit.h"
 
 void fcn_to_minimize(int& npar, double* deriv, double& f, double par[], int flag)
 {
-  TFile * f_bi214 = TFile::Open("bi214_pdf.root");
-  TH1F *bi214_pdf = (TH1F*)f_bi214->Get("1e1g_electron_gamma_energy_sum");
-
-  TFile * f_tl208 = TFile::Open("tl208_pdf.root");
-  TH1F *tl208_pdf = (TH1F*)f_tl208->Get("1e1g_electron_gamma_energy_sum");
-
   TFile * f_pseudo = TFile::Open("pseudo.root");
-  TH1F *pseudo = (TH1F*)f_pseudo->Get("pseudo");
 
   f = 0.;
-  for(int i = 0; i < 100; i++)
-    {
-      double b = (par[0]*bi214_pdf->GetBinContent(i)*bi214_channel_1e1g_efficiency
-                  +par[1]*tl208_pdf->GetBinContent(i)*tl208_channel_1e1g_efficiency)*7*3.14e7*2.5;
-      double d = pseudo->GetBinContent(i);
 
-      if(b==0)
-        b=1e-6;
-      std::cout << "i b d " << i << "  " << b << "  " << d << std::endl;
-      f += 2*(b-d*log(b)+log_factorial(d));
-    }
+  for(auto j = quantities.begin(); j != quantities.end(); ++j) {
 
-  f_bi214->Close();
-  f_tl208->Close();
+    for(int i = 0; i < 100; i++)
+      {
+        double b = 0;
+        unsigned int count = 0;
+        for(auto k = isotope_activity.begin(); k != isotope_activity.end(); ++k) {
+          TString channel = k->first + "_" + *j;
+          double efficiency = 0.1;
+          // double efficiency = quantity_efficiency.at(channel);
+          TString pdf_file = k->first + "_pdf.root";
+          TFile * f = TFile::Open(pdf_file);
+          TH1F *h = (TH1F*)f->Get(channel);
+
+          b += par[count] * efficiency * h->GetBinContent(i);
+          count++;
+          f->Close();
+        }
+        TH1F *pseudo = (TH1F*)f_pseudo->Get(*j);
+        double d = pseudo->GetBinContent(i);
+
+        if(b==0)
+          b=1e-6;
+        std::cout << "i b d " << i << "  " << b << "  " << d << std::endl;
+        f += 2*(b-d*log(b)+log_factorial(d));
+      }
+  }
+
   f_pseudo->Close();
   return;
 }
@@ -79,133 +74,131 @@ void fcn_to_minimize(int& npar, double* deriv, double& f, double par[], int flag
 //   return f;
 // }
 
-void multi_fit()
+void multi_fit(std::map < std::string, std::vector<double> > & activity_measurement)
 {
+  // gSystem->Load("/home/calvez/nemo/work_dir/Cadfael/build/ports/root/root-prefix/src/root/lib/libMinuit.so");
+
   const int npar = 2;
 
   TMinuit minuit(npar);
 
-  minuit.SetFCN(fcn_to_minimize);  //set the fcn to minimize
+  // minuit.SetFCN(fcn_to_minimize);  //set the fcn to minimize
 
-  double par[npar];               // the start values
-  double stepSize[npar];          // step sizes
-  double minVal[npar];            // minimum bound on parameter
-  double maxVal[npar];            // maximum bound on parameter
-  string parName[npar];           // parameter name
+  // double par[npar];               // the start values
+  // double stepSize[npar];          // step sizes
+  // double minVal[npar];            // minimum bound on parameter
+  // double maxVal[npar];            // maximum bound on parameter
+  // std::string parName[npar];           // parameter name
 
-  par[0]= 10./1e6;
-  stepSize[0] = 1e-6;
-  minVal[0] = 1e-9;
-  maxVal[0] = 1e-3;
-  // parName[0] = "activity in uBq/kg";
-  parName[0] = "activity bi214";
+  // unsigned int count = 0;
+  // for(auto i = isotope_activity.begin(); i != isotope_activity.end(); ++i) {
+  //   par[count] = 20./1e6;
+  //   stepSize[count] = 1e-7;
+  //   minVal[count] = 1e-9;
+  //   maxVal[count] = 1e-3;
+  //   parName[count] = "activity in " + i->first + "in uBq/kg";
+  //   count++;
 
-  minuit.DefineParameter(0, parName[0].c_str(),
-                         par[0], stepSize[0], minVal[0], maxVal[0]);
-
-  par[1]= 2./1e6;
-  stepSize[1] = 1e-6;
-  minVal[1] = 1e-9;
-  maxVal[1] = 1e-3;
-  // parName[1] = "activity in uBq/kg";
-  parName[1] = "activity tl208";
-
-  minuit.DefineParameter(1, parName[1].c_str(),
-                         par[1], stepSize[1], minVal[1], maxVal[1]);
-
-  minuit.Migrad();
-
-  double activity_bi214 = 0;
-  double activity_bi214_err = 0;
-  minuit.GetParameter(0,activity_bi214,activity_bi214_err);
-
-  double activity_tl208 = 0;
-  double activity_tl208_err = 0;
-  minuit.GetParameter(1,activity_tl208,activity_tl208_err);
-
-  std::cout << " Measured bi214 activity is : " << activity_bi214*1e6 << " +/- " << activity_bi214_err*1e6 << " uBq/kg" << std::endl;
-  std::cout << " Measured tl208 activity is : " << activity_tl208*1e6 << " +/- " << activity_tl208_err*1e6 << " uBq/kg" << std::endl;
-
-  // TGraph *g_likelihood = new TGraph(100);
-  // double activity_start = 1e-5;
-  // double activity_end = 1e-3;
-
-  // for (unsigned int i = 0; i<100;++i) {
-  //   double activity = activity_start + i*(activity_end-activity_start)/100.;
-  //   double Q = likelihood(activity);
-  //   g_likelihood->SetPoint(i,activity*1e6,Q);
+  //   minuit.DefineParameter(count, parName[count].c_str(),
+  //                          par[count], stepSize[count], minVal[count], maxVal[count]);
   // }
-  // g_likelihood->Draw();
 
-  TFile * f_bi214 = TFile::Open("bi214_pdf.root");
-  TH1F *bi214_pdf = (TH1F*)f_bi214->Get("1e1g_electron_gamma_energy_sum");
+  // minuit.Migrad();
 
-  bi214_pdf->SetLineColor(kOrange);
-  bi214_pdf->SetFillColor(kOrange);
+  // unsigned int count_bis = 0;
+  // for(auto i = isotope_activity.begin(); i != isotope_activity.end(); ++i) {
+  //   double activity = 0;
+  //   double activity_err = 0;
+  //   minuit.GetParameter(count,activity,activity_err);
+  //   activity_measurement.insert(std::pair<std::string,std::vector<double>>(i->first,{activity,activity_err}));
+  //   count_bis++;
+  // }
 
-  TFile * f_tl208 = TFile::Open("tl208_pdf.root");
-  TH1F *tl208_pdf = (TH1F*)f_tl208->Get("1e1g_electron_gamma_energy_sum");
-  tl208_pdf->SetLineColor(kGreen+1);
-  tl208_pdf->SetFillColor(kGreen+1);
+  // // std::cout << " Measured bi214 activity is : " << activity_bi214*1e6 << " +/- " << activity_bi214_err*1e6 << " uBq/kg" << std::endl;
+  // // std::cout << " Measured tl208 activity is : " << activity_tl208*1e6 << " +/- " << activity_tl208_err*1e6 << " uBq/kg" << std::endl;
 
-  TFile * f_pseudo = TFile::Open("pseudo.root");
-  TH1F *pseudo = (TH1F*)f_pseudo->Get("pseudo");
-  pseudo->SetLineColor(kBlack);
-  // pseudo->SetMarkerStyle(1);
+  // // TGraph *g_likelihood = new TGraph(100);
+  // // double activity_start = 1e-5;
+  // // double activity_end = 1e-3;
 
-  THStack *hs = new THStack("hs","Energy [keV]");
-  bi214_pdf->Scale(activity_bi214*bi214_channel_1e1g_efficiency*mass*exposure);
-  tl208_pdf->Scale(activity_tl208*tl208_channel_1e1g_efficiency*mass*exposure);
+  // // for (unsigned int i = 0; i<100;++i) {
+  // //   double activity = activity_start + i*(activity_end-activity_start)/100.;
+  // //   double Q = likelihood(activity);
+  // //   g_likelihood->SetPoint(i,activity*1e6,Q);
+  // // }
+  // // g_likelihood->Draw();
 
-  hs->Add(bi214_pdf);
-  hs->Add(tl208_pdf);
 
-  TCanvas *c1 = new TCanvas("c1","example",600,700);
 
-  TPad *pad1 = new TPad("pad1","pad1",0,0.3,1,1);
-  // pad1->SetBottomMargin(0.05);
-  pad1->SetTopMargin(0.03);
-  pad1->Draw();
-  pad1->cd();
 
-  hs->DrawClone();
+  // // //tmp
+  // // double bi214_channel_1e1g_efficiency = 0.1;
+  // // double tl208_channel_1e1g_efficiency = 0.1;
 
-  pseudo->DrawClone("samePE");
-  c1->cd();
+  // // TFile * f_bi214 = TFile::Open("bi214_pdf.root");
+  // // TH1F *bi214_pdf = (TH1F*)f_bi214->Get("1e1g_electron_gamma_energy_sum");
 
-  // TH1 *h_sum = new TH1F("h_sum","h_sum");
+  // // bi214_pdf->SetLineColor(kOrange);
+  // // bi214_pdf->SetFillColor(kOrange);
 
-  bi214_pdf->Sumw2();
-  bi214_pdf->Add(tl208_pdf);
-  //Error computation to check
-  // bi214_pdf->Sumw2();
-  pseudo->Sumw2();
+  // // TFile * f_tl208 = TFile::Open("tl208_pdf.root");
+  // // TH1F *tl208_pdf = (TH1F*)f_tl208->Get("1e1g_electron_gamma_energy_sum");
+  // // tl208_pdf->SetLineColor(kGreen+1);
+  // // tl208_pdf->SetFillColor(kGreen+1);
 
-  TPad *pad2 = new TPad("pad2","pad2",0,0,1,0.3);
-  pad2->SetTopMargin(0.0);
-  // pad2->SetBottomMargin(0.05);
-  pad2->Draw();
-  pad2->cd();
+  // // TFile * f_pseudo = TFile::Open("pseudo.root");
+  // // TH1F *pseudo = (TH1F*)f_pseudo->Get("pseudo");
+  // // pseudo->SetLineColor(kBlack);
+  // // // pseudo->SetMarkerStyle(1);
 
-  bi214_pdf->SetStats(0);
-  pseudo->SetStats(0);
+  // // THStack *hs = new THStack("hs","Energy [keV]");
+  // // bi214_pdf->Scale(activity_bi214*bi214_channel_1e1g_efficiency*mass*exposure);
+  // // tl208_pdf->Scale(activity_tl208*tl208_channel_1e1g_efficiency*mass*exposure);
 
-  pseudo->Divide(bi214_pdf);
+  // // hs->Add(bi214_pdf);
+  // // hs->Add(tl208_pdf);
 
-  pseudo->GetYaxis()->SetRangeUser(0.6,1.4);
-  pseudo->Draw("ep");
+  // // TCanvas *c1 = new TCanvas("c1","example",600,700);
 
-  TLine *line = new TLine(0,1,5,1);
-  // line->SetLineColor(kBlack);
-  line->SetLineStyle(kDashed);
-  line->Draw("same");
+  // // TPad *pad1 = new TPad("pad1","pad1",0,0.3,1,1);
+  // // // pad1->SetBottomMargin(0.05);
+  // // pad1->SetTopMargin(0.03);
+  // // pad1->Draw();
+  // // pad1->cd();
 
-  c1->cd();
+  // // hs->DrawClone();
+
+  // // pseudo->DrawClone("samePE");
+  // // c1->cd();
+
+  // // // TH1 *h_sum = new TH1F("h_sum","h_sum");
+
+  // // bi214_pdf->Sumw2();
+  // // bi214_pdf->Add(tl208_pdf);
+  // // //Error computation to check
+  // // // bi214_pdf->Sumw2();
+  // // pseudo->Sumw2();
+
+  // // TPad *pad2 = new TPad("pad2","pad2",0,0,1,0.3);
+  // // pad2->SetTopMargin(0.0);
+  // // // pad2->SetBottomMargin(0.05);
+  // // pad2->Draw();
+  // // pad2->cd();
+
+  // // bi214_pdf->SetStats(0);
+  // // pseudo->SetStats(0);
+
+  // // pseudo->Divide(bi214_pdf);
+
+  // // pseudo->GetYaxis()->SetRangeUser(0.6,1.4);
+  // // pseudo->Draw("ep");
+
+  // // TLine *line = new TLine(0,1,5,1);
+  // // // line->SetLineColor(kBlack);
+  // // line->SetLineStyle(kDashed);
+  // // line->Draw("same");
+
+  // // c1->cd();
 
   return;
-}
-
-int main()
-{
-  multi_fit();
 }
--- sensitivity/multi_fit.h
+++ sensitivity/multi_fit.h
@@ -1,6 +1,10 @@
 #ifndef MULTI_FIT_H
 #define MULTI_FIT_H 1
 
-void multi_fit();
+#include <vector>
+#include <string>
+#include <map>
+
+void multi_fit(std::map < std::string, std::vector<double> > & activity_measurement);
 
 #endif
